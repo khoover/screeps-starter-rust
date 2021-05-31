3f19763c99d700216e4ba852b7b936ca56a3561e
Ken Hoover <khooveri@eng.ucsd.edu>
2021-05-30 23:46:36 -0700

    A running example, just use 'cd dist; node ./test-rollup.js'.

--- javascript/main.js
+++ javascript/main.js
@@ -1,37 +1,28 @@
 "use strict";
-let wasm_module;
+let wasm_module = Game.cpu.bucket >= 500 ? new WebAssembly.Module(require('screeps_starter_rust_bg')) : null;
+import init from '../rust-out/screeps_starter_rust.js';
+let wasm = Game.cpu.bucket >= 500 ? init(wasm_module) : null;
 
 function console_error(...args) {
     console.log(...args);
     Game.notify(args.join(' '));
 }
 
-module.exports.loop = function() {
+export const loop = function() {
     try {
-        if (wasm_module && wasm_module.__wasm) {
-            wasm_module.loop();
-        } else {
-            // attempt to load the wasm only if there's enough bucket to do a bunch of work this tick
-            if (Game.cpu.bucket < 500) {
-                console.log("we are running out of time, pausing compile!" + JSON.stringify(Game.cpu));
-                return;
-            }
-            
-            // replace this initialize function on the module
-            wasm_module = require("screeps-starter-rust");
-            // load the wasm instance!
-            wasm_module.initialize_instance();
-            // run the setup function, which configures logging
-            wasm_module.setup();
-            // go ahead and run the loop for its first tick
-            wasm_module.loop();
+        if (!wasm) {
+            wasm = init(wasm_module);
         }
+        wasm.make_call();
+        wasm = null;
     } catch(error) {
         console_error("caught exception:", error);
         if (error.stack) {
             console_error("stack trace:", error.stack);
         }
         console_error("resetting VM next tick.");
-        wasm_module.__wasm = null;
+        wasm = null;
     }
 }
+
+loop();
