3747037d03082bb4a6aa635e76715a16c039e80a
Ken Hoover <k.hoover3@gmail.com>
2022-03-10 09:06:00 -0800

    Added a version of the deser with as_chunks usage, slightly faster.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![feature(test)]
 #![feature(bench_black_box)]
+#![feature(slice_as_chunks)]
 
 #[macro_use]
 extern crate lazy_static;
@@ -7,7 +8,7 @@ extern crate lazy_static;
 use wasm_bindgen::prelude::*;
 use web_sys::console;
 use js_sys::JsString;
-use std::convert::TryInto;
+use std::convert::{TryInto, TryFrom};
 
 macro_rules! console_log {
     // Note that this is using the `log` function imported above during
@@ -150,9 +151,64 @@ fn bytestring_to_utf15(bytes: &[u8]) -> Vec<u16> {
     output
 }
 
+#[inline(always)]
+fn faster_write_chunk(bytes: &[u8; 8], extras: u64, output_chunk: &mut [u16; 4]) -> u64 {
+    let byte_u64 = u64::from_ne_bytes(*bytes);
+    output_chunk[0] = ((byte_u64 >> 4) & LOW_FIFTEEN_BITS) as u16;
+    output_chunk[1] = ((byte_u64 >> 19) & LOW_FIFTEEN_BITS) as u16;
+    output_chunk[2] = ((byte_u64 >> 34) & LOW_FIFTEEN_BITS) as u16;
+    output_chunk[3] = ((byte_u64 >> 49) & LOW_FIFTEEN_BITS) as u16;
+    (extras | (byte_u64 & LOW_FOUR_BITS)) << 4
+}
+
+fn alt_bytestring_to_utf15(bytes: &[u8]) -> Vec<u16> {
+    if bytes.is_empty() { return Vec::new(); }
+            
+    let (fast_input_blocks, remainder): (&[[u8; INPUT_BLOCK_SIZE]], &[u8]) = bytes.as_chunks();
+    let has_remainder = !remainder.is_empty();
+    let encoded_len = 1+OUTPUT_BLOCK_SIZE*(fast_input_blocks.len() + if has_remainder { 1 } else { 0 });
+    
+    let mut output: Vec<u16> = vec![0; encoded_len];
+    if has_remainder { output[0] = 0x0031; } else { output[0] = 0x0030; }
+    // SAFETY: output.len is 1 plus a multiple of OUTPUT_BLOCK_SIZE.
+    let output_blocks: &mut [[u16; OUTPUT_BLOCK_SIZE]] = unsafe { output[1..].as_chunks_unchecked_mut() };
+    
+    let maybe_remainder: Option<[u8; INPUT_BLOCK_SIZE]> = if has_remainder {
+        let mut fixed_remainder = [0u8; INPUT_BLOCK_SIZE];
+        let len = remainder.len();                                                                                                                                                                                                                                                                                                       
+        fixed_remainder[0] = remainder.len() as u8;
+        fixed_remainder[1..1+len].copy_from_slice(remainder);
+        Some(fixed_remainder)
+    } else {
+        None
+    };
+    
+    output_blocks.iter_mut().zip(fast_input_blocks.iter().chain(&maybe_remainder)).for_each(|(output_block, input_block)| {
+        //SAFETY: INPUT_BLOCK_SIZE is a multiple of 8.
+        let input_groups: &[[u8; 8]] = unsafe { input_block.as_chunks_unchecked() };
+        //SAFETY: OUTPUT_BLOCK_SIZE is a multiple of 4.
+        let output_groups: &mut [[u16; 4]] = unsafe { output_block.as_chunks_unchecked_mut() };
+        let extras = output_groups.iter_mut().zip(input_groups).fold(0u64, |extras, (output_group, input_group)| {
+            faster_write_chunk(input_group, extras, output_group)
+        });
+
+        output_block[60] = ((extras >> 4) & LOW_FIFTEEN_BITS) as u16;
+        output_block[61] = ((extras >> 19) & LOW_FIFTEEN_BITS) as u16;                                                                                                                                                                                                                                                      
+        output_block[62] = ((extras >> 34) & LOW_FIFTEEN_BITS) as u16;                                                                                                                                                                                                                                                               output_block[63] = ((extras >> 49) & LOW_FIFTEEN_BITS) as u16;
+    });
+    
+    output
+}
+
 #[wasm_bindgen]
 pub fn bytestring_to_jsstring(bytes: &[u8]) -> JsString {
     let utf15 = bytestring_to_utf15(bytes);
+    utf16_to_jsstring(utf15.as_slice())
+}
+
+#[wasm_bindgen]
+pub fn alt_bytestring_to_jsstring(bytes: &[u8]) -> JsString {
+    let utf15 = alt_bytestring_to_utf15(bytes);
     utf16_to_jsstring(utf15.as_slice())
 }
 
@@ -277,6 +333,117 @@ pub fn jsstring_to_bytestring(utf15: &JsString) -> Vec<u8> {
     output
 }
 
+#[inline(always)]
+fn alt_write_u64(num: u64, buf: &mut [u8; 8]) {
+    let bytes = u64::to_ne_bytes(num);
+    for i in 0..8 {
+        buf[i] = bytes[i];
+    }
+}
+
+#[inline(always)]
+fn alt_decode_block(input: &[u16; 4], remainder: u64, output: &mut [u8; 8]) {
+    let mut accum = remainder;
+
+    accum |= (input[0] as u64) << 4;
+    accum |= (input[1] as u64) << 19;
+    accum |= (input[2] as u64) << 34;
+    accum |= (input[3] as u64) << 49;
+
+    alt_write_u64(accum, output);
+}
+
+#[inline(always)]
+fn alt_decode_extra(input: &[u16; 4]) -> u64 {
+    let mut accum: u64 = input[0] as u64;
+    accum |= (input[1] as u64) << 15;
+    accum |= (input[2] as u64) << 30;
+    accum |= (input[3] as u64) << 45;
+    accum
+}
+
+#[wasm_bindgen]
+pub fn alt_jsstring_to_bytestring(utf15: &JsString) -> Vec<u8> {
+    if utf15.length() == 0 { return Vec::new(); }
+    assert_eq!(utf15.length() % OUTPUT_BLOCK_SIZE as u32, 1);
+
+    // SAFETY: TryFrom<u32> for usize is always Ok.
+    let mut utf15_vec: Vec<u16> = vec![0; unsafe { utf15.length().try_into().unwrap_unchecked() }];
+    jsstring_to_utf16(&utf15, utf15_vec.as_mut_slice()).unwrap();
+    let first = utf15_vec[0];
+    assert!(first == 0x0030 || first == 0x0031);
+    let utf15_vec = &utf15_vec[1..];
+    debug_assert!(utf15_vec.iter().max().unwrap() & 0x8000 == 0);
+
+    let has_remainder = first == 0x0031;
+    let (fast_blocks, maybe_remainder, remainder_size): (&[[u16; OUTPUT_BLOCK_SIZE]], Option<&[u16; OUTPUT_BLOCK_SIZE]>, usize) = {
+        // SAFETY: we removed one element of utf15_vec, and checked that the original length was a
+        // multiple of OUTPUT_BLOCK_SIZE plus 1.
+        let chunks: &[[u16; OUTPUT_BLOCK_SIZE]] = unsafe { utf15_vec.as_chunks_unchecked() };
+        if has_remainder {
+            let remainder = &chunks[chunks.len() - 1];
+            let mut buf: [u8; 8] = [0; 8];
+            let extra = (remainder[63] >> 11) as u64 & LOW_FOUR_BITS;
+            // SAFETY: Immediate.
+            alt_decode_block(unsafe { <&[u16; 4]>::try_from(&remainder[0..4]).unwrap_unchecked() }, extra, &mut buf);
+            (&chunks[..chunks.len() - 1], Some(remainder), buf[0] as usize)
+        } else {
+            (chunks, None, 0)
+        }
+    };
+    let mut output: Vec<u8> = vec![0; INPUT_BLOCK_SIZE * fast_blocks.len() + remainder_size];
+    let (output_blocks, output_remainder): (&mut [[u8; INPUT_BLOCK_SIZE]], &mut[u8]) = output.as_chunks_mut();
+
+    fast_blocks.iter().zip(output_blocks.iter_mut()).for_each(|(utf15_block, byte_block)| {
+        // SAFETY: INPUT_BLOCK_SIZE % 8 == 0
+        let byte_groups: &mut [[u8; 8]] = unsafe { byte_block.as_chunks_unchecked_mut() };
+        // SAFETY: OUTPUT_BLOCK_SIZE % 4 == 0
+        let utf15_groups: &[[u16; 4]] = unsafe { utf15_block.as_chunks_unchecked() };
+        let mut utf15_iter = utf15_groups.iter();
+        // SAFETY: utf15_groups is never empty;
+        let extra = alt_decode_extra(unsafe { utf15_iter.next_back().unwrap_unchecked() });
+        utf15_iter.zip(byte_groups.iter_mut()).rfold(extra, |extra, (utf15_group, byte_group)| {
+            alt_decode_block(utf15_group, extra & LOW_FOUR_BITS, byte_group);
+            extra >> 4
+        });
+    });
+
+    if let Some(input_remainder) = maybe_remainder {
+        // SAFETY: OUTPUT_BLOCK_SIZE % 4 == 0.
+        let input_groups: &[[u16; 4]] = unsafe { input_remainder.as_chunks_unchecked() };
+        let mut tmp: [u8; 8] = [0; 8];
+        let extra = alt_decode_extra(&input_groups[input_groups.len() - 1]);
+
+        alt_decode_block(&input_groups[0], (extra >> 56) & LOW_FOUR_BITS, &mut tmp);
+        output_remainder[..7.min(remainder_size)].copy_from_slice(&tmp[1..8.min(remainder_size+1)]);
+        if remainder_size <= 7 { return output; }
+
+        let (semi_fast_bytes, annoying_bytes): (&mut [[u8; 8]], &mut [u8]) = output_remainder[7..].as_chunks_mut();
+        let input_groups = &input_groups[1..];
+        if !semi_fast_bytes.is_empty() {
+            let extra_shift = input_groups.iter().zip(semi_fast_bytes.iter_mut()).fold(52u8, |extra_shift, (utf15_group, byte_group)| {
+                alt_decode_block(utf15_group, (extra >> extra_shift) & LOW_FOUR_BITS, byte_group);
+                extra_shift - 4
+            });
+            // SAFETY: We know output_remainder.len() < INPUT_BLOCK_SIZE, so in particular:
+            // semi_fast_bytes.len() = (output_reminder.len() - 7) / 8 < INPUT_BLOCK_SIZE / 8 - 1 <
+            // OUTPUT_BLOCK_SIZE / 4 = input_groups.len().
+            let annoying_input_group = unsafe { input_groups.get_unchecked(semi_fast_bytes.len()) };
+            alt_decode_block(annoying_input_group, (extra >> extra_shift) & LOW_FOUR_BITS, &mut tmp);
+            for i in 0..annoying_bytes.len() {
+                annoying_bytes[i] = tmp[i];
+            }
+        } else if !annoying_bytes.is_empty() {
+            alt_decode_block(&input_groups[0], (extra >> 52) & LOW_FOUR_BITS, &mut tmp);
+            for i in 0..annoying_bytes.len() {
+                annoying_bytes[i] = tmp[i];
+            }
+        }
+    }
+
+    output
+}
+
 use std::{cell::RefCell, sync::RwLock, collections::HashMap};
 thread_local! {
     static TL_CELL: RefCell<HashMap<u8, u8>> = RefCell::new(HashMap::new());
@@ -346,12 +513,11 @@ pub fn make_call() {
     use getrandom::getrandom;
     use base64::{decode, encode_config_slice, STANDARD};
     use std::time::Duration;
-    use std::convert::TryFrom;
     use js_sys::Date;
 
     const RUNS: usize = 1000;
     const NINE_NINE_INDEX: usize = (RUNS / 100) * 99 - 1;
-    const BYTES: usize = 3*1024*1024;
+    const BYTES: usize = 1024*1024;
 
     let start = Date::now();
     for _ in 1 ..= 100_000_000 {
@@ -370,6 +536,8 @@ pub fn make_call() {
     let mut bytes = vec![0; BYTES];
     let mut ser_times: Vec<Duration> = Vec::with_capacity(RUNS);
     let mut de_times: Vec<Duration> = Vec::with_capacity(RUNS);
+    let mut alt_ser_times: Vec<Duration> = Vec::with_capacity(RUNS);
+    let mut alt_de_times: Vec<Duration> = Vec::with_capacity(RUNS);
     let mut b64_de_times: Vec<Duration> = Vec::with_capacity(RUNS);
     let mut b64_slice_ser_times: Vec<Duration> = Vec::with_capacity(RUNS);
     let mut dumb_ser_times: Vec<Duration> = Vec::with_capacity(RUNS);
@@ -387,6 +555,15 @@ pub fn make_call() {
         let end = Date::now();
         ser_times.push(Duration::from_secs_f64((mid - start)/1000f64));
         de_times.push(Duration::from_secs_f64((end - mid) / 1000f64));
+        assert_eq!(back, bytes);
+
+        let start = Date::now();
+        let s = alt_bytestring_to_jsstring(&bytes);
+        let mid = Date::now();
+        let back = alt_jsstring_to_bytestring(&s);
+        let end = Date::now();
+        alt_ser_times.push(Duration::from_secs_f64((mid - start)/1000f64));
+        alt_de_times.push(Duration::from_secs_f64((end - mid) / 1000f64));
         assert_eq!(back, bytes);
 
         let start = Date::now();
@@ -426,6 +603,15 @@ pub fn make_call() {
     let de_avg: Duration = de_times.iter().sum::<Duration>() / (RUNS as u32);
     console_log!("SERIALIZE: Max {:?} -- Min {:?} -- Avg {:?} -- 99% {:?}", ser_max, ser_min, ser_avg, ser_99);
     console_log!("DESERIALIZE: Max {:?} -- Min {:?} -- Avg {:?} -- 99% {:?}", de_max, de_min, de_avg, de_99);
+
+    alt_ser_times.sort();
+    let (alt_ser_max, alt_ser_min, alt_ser_99) = (alt_ser_times[RUNS-1], alt_ser_times[0], alt_ser_times[NINE_NINE_INDEX]);
+    let alt_ser_avg: Duration = alt_ser_times.iter().sum::<Duration>() / (RUNS as u32);
+    alt_de_times.sort();
+    let (alt_de_max, alt_de_min, alt_de_99) = (alt_de_times[RUNS-1], alt_de_times[0], alt_de_times[NINE_NINE_INDEX]);
+    let alt_de_avg: Duration = alt_de_times.iter().sum::<Duration>() / (RUNS as u32);
+    console_log!("ALT SERIALIZE: Max {:?} -- Min {:?} -- Avg {:?} -- 99% {:?}", alt_ser_max, alt_ser_min, alt_ser_avg, alt_ser_99);
+    console_log!("ALT DESERIALIZE: Max {:?} -- Min {:?} -- Avg {:?} -- 99% {:?}", alt_de_max, alt_de_min, alt_de_avg, alt_de_99);
 
     b64_slice_ser_times.sort();
     let (b64_slice_ser_max, b64_slice_ser_min, b64_slice_ser_99) = (b64_slice_ser_times[RUNS-1], b64_slice_ser_times[0], b64_slice_ser_times[NINE_NINE_INDEX]);
